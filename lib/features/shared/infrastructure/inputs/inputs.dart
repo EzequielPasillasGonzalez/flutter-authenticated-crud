4ff540239c1721e71d37b4674856a677bb93fe08
Ezequiel <ingepasillasg@gmail.com>
2026-03-06 09:15:59 -0600

    feat: errores personalizados para el producto

--- lib/features/shared/infrastructure/inputs/inputs.dart
+++ lib/features/shared/infrastructure/inputs/inputs.dart
@@ -2,3 +2,7 @@ export './email.dart';
 export './password.dart';
 export './confirm_password.dart';
 export './name.dart';
+export 'price.dart';
+export 'stock.dart';
+export 'slug.dart';
+export 'slug.dart';
